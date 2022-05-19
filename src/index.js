424e43a437b541269e015bd0a406c7f4ccd28c3f
OMTut <j_tuttle04@hotmail.com>
2022-05-19 08:37:35 -0500

    added the get method

--- src/index.js
+++ src/index.js
@@ -7,3 +7,23 @@ app.listen(PORT, () => console.log(
     `Running Express Server on Port ${PORT}`
     )
 );
+
+//Get method takes 2 params
+//Path and Callback function. The callback function has 2 params
+//req (client sends data) and response (server handles data and returns)
+app.get('/groceries', (req, res) => {
+    res.send([
+        {
+            item: 'milk',
+            quantity: 2,
+        },
+        {
+            item: 'cereal',
+            quantity: 1,
+        },
+        {
+            item: 'bananas',
+            quantity: 1,
+        },
+    ])
+})
